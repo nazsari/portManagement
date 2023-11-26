4b029332d8713f681ed43f4a8b232d35910d43c6
Nazik Sari Gokpur <37064140+nazsari@users.noreply.github.com>
2023-11-26 22:21:58 +0300

    version1

---
+++ src/main/java/com/portManagement/PortManagementApplication.java
@@ -0,0 +1,13 @@
+package com.portManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portManagement/PortManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
